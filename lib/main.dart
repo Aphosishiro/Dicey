988bdf28cd8392fd9c6212bb7cf9efdd9233f8b0
Aphosishiro <Aphosishiro@gmail.com>
2022-10-05 22:26:52 +0100

    added string counter feature

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,16 @@ class Dicey extends StatefulWidget {
 
 int leftdicenumber = 1;
 int rightdicenumber = 1;
+int roll = 2;
 
 class _DiceyState extends State<Dicey> {
-  
-void rolldice() {
-  setState(() {
-    leftdicenumber = Random().nextInt(6) + 1;
-    rightdicenumber = Random().nextInt(6) + 1;
-  });
-}
+  void rolldice() {
+    setState(() {
+      leftdicenumber = Random().nextInt(6) + 1;
+      rightdicenumber = Random().nextInt(6) + 1;
+      roll = leftdicenumber + rightdicenumber;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,31 +47,65 @@ void rolldice() {
           centerTitle: true,
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center, 
           children: [
-            Row(
-              children: [
-                Expanded(
-                    child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Image.asset("asset/Dice$leftdicenumber.png"),
-                )),
-                Expanded(
-                    child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Image.asset("asset/Dice$rightdicenumber.png"),
-                )),
-              ],
+            const SizedBox(
+            height: 30,
+          ),
+          Row(
+            children: [
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Image.asset("asset/Dice$leftdicenumber.png"),
+              )),
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Image.asset("asset/Dice$rightdicenumber.png"),
+              )),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            "You rolled",
+            style: TextStyle(
+              fontFamily: "Pacifico",
+              fontSize: 30,
             ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            rolldice();
-          },
-          backgroundColor: Colors.black,
-          child: const Icon(Icons.play_arrow),
-        ),
+          ),
+          Text(
+            "$roll",
+            style: const TextStyle(
+              fontFamily: "Pacifico",
+              fontSize: 50,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            height: 70,
+            width: 200,
+            child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                  elevation: MaterialStateProperty.all(5),
+                ),
+                onPressed: () {
+                  rolldice();
+                },
+                child: const Text(
+                  "ROLL",
+                  style: TextStyle(
+                    fontFamily: "Poppins",
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                ),
+          ),
+        ]),
       ),
     );
   }
